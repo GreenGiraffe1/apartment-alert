d0bbd1657cff27161b65c231200afffa5a5d0cc5
Matthew Hedlund <mhedlund@wayfair.com>
2019-01-26 18:13:45 -0500

    Add template file for necessary constants

---
+++ constants(TEMPLATE).py
@@ -0,0 +1,24 @@
+"""
+Confidential & credential information needed for the alert script
+
+"""
+
+
+# TODO: Add you your values below, and remove "(TEMPLATE)" from the name of this file
+
+
+# Apartment Website URL
+SITE_URL =
+
+
+# MainGun Email Provider Info
+EMAIL_ADDRESS =
+MAILGUN_API_KEY =
+MAILGUN_DOMAIN_NAME =
+
+
+# Twilio SMS Provider Info
+TWILIO_SID =
+TWILIO_AUTH_TOKEN =
+FROM_PHONE =
+TO_PHONE =
